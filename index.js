6686165ac6b71bbed08f9c1828c600a74c17085e
Rashad <87971037+RashadM0@users.noreply.github.com>
2023-04-11 14:02:33 +0400

    ClassworkAdded

---
+++ index.js
@@ -0,0 +1,87 @@
+////1
+// let num = 3;
+
+// if (num > 10) {
+//   console.log("boyukdur");
+// } else console.log("kicikdir");
+
+// if (num == 10) {
+//   console.log("beraberdir");
+// }
+
+// //2
+
+// if (num % 2 == 0) {
+//   console.log("cut");
+// } else console.log("tek");
+
+// //3
+
+// let sum = 1;
+
+// for (let i = 1; i <= 100; i++) {
+//   sum += i;
+// }
+// console.log(sum);
+
+////4
+
+// let monthCount = 1;
+// let mounthName;
+
+// switch (monthCount) {
+//   case 1:
+//     mounthName = "ynv";
+//     break;
+//   case 2:
+//     mounthName = "fbr";
+//     break;
+//   case 3:
+//     mounthName = "mrch";
+//     break;
+//   case 4:
+//     mounthName = "apr";
+//     break;
+//   case 5:
+//     mounthName = "may";
+//     break;
+//   case 6:
+//     mounthName = "june";
+//     break;
+//   case 7:
+//     mounthName = "july";
+//     break;
+//   case 8:
+//     mounthName = "aug";
+//     break;
+//   case 9:
+//     mounthName = "sep";
+//     break;
+//   case 10:
+//     mounthName = "oct";
+//     break;
+//   case 11:
+//     mounthName = "nov";
+//     break;
+//   case 12:
+//     mounthName = "dec";
+//     break;
+//   default:
+//     break;
+// }
+// console.log(mounthName);
+
+////5
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 == 0 && i % 5 == 0) {
+//     console.log(i, "fizzbuzz");
+//   } else if (i % 5 == 0) {
+//     console.log(i, "buzz");
+//   } else if (i % 3 == 0) {
+//     console.log(i, "fizz");
+//   }
+// }
+
+////6
+
